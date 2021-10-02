ab7bd0c70fc2b5595d1486ca188f94304a62d82b
Joao Milone <j.milone@pipastudios.com>
2021-10-02 20:27:03 -0300

    Update Translator_TextMeshPro.cs

--- TranslationSystem_Scripts/Translator_TextMeshPro.cs
+++ TranslationSystem_Scripts/Translator_TextMeshPro.cs
@@ -2,18 +2,18 @@ using TMPro;
 using TranslationSystem.Base;
 using UnityEngine;
 
-[RequireComponent(typeof(TextMeshPro))]
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class Translator_TextMeshPro : MonoBehaviour
 {
     [SerializeField] private string textKey = "title";
     [SerializeField] private TranslateFormat translateFormat = TranslateFormat.Standard;
 
-    private TextMeshPro textFieldToTranslate;
+    private TextMeshProUGUI textFieldToTranslate;
 
     private void OnEnable()
     {
         if (textFieldToTranslate == null)
-            textFieldToTranslate = GetComponent<TextMeshPro>();
+            textFieldToTranslate = GetComponent<TextMeshProUGUI>();
 
         Translate.OnLanguageChanged += GetTranslatedText;
         GetTranslatedText();
